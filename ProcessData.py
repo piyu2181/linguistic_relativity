11b72ed6789edc80f91bee664beeb8ad6c881047
Deepak Gupta <dgupta@Deepaks-MacBook-Pro.local>
2019-03-10 15:09:49 +0100

    Adding processing class related functionalities

--- ProcessData.py
+++ ProcessData.py
@@ -8,5 +8,30 @@ Year: 2019
 
 class ProcessData:
 
-    # Defining the attributes of the class
-    min_count = 5 # Defines the minimum number of documents that a certain word needs to be present to be considered for study
+    '''
+    Attributes:
+    min_count: Defines the min. number of documents that contain a certain word; a count below this number would
+    ignore the word from the study
+    remove_n:
+    n_topics:
+    n_passes:
+    '''
+    min_count = 5
+    remove_n = 50
+    n_topics = 10
+    n_passes = 20
+
+    def __init__(self, languages, years, min_count = 5, remove_n = 50, n_topics = 10, n_passes = 20):
+
+        self.languages = languages
+        self.years = years
+        self.min_count = min_count
+        self.remove_n = remove_n
+        self.n_topics = n_topics
+        self.n_passes = n_passes
+
+
+    def find_matching_files(self):
+
+
+
