7ba56fbf8b851c508d79297446cf63a16f061021
Debjani Bhowmick <debjani@Debjanis-MacBook-Air.local>
2019-03-10 17:34:10 +0100

    adding matching file method

--- ProcessData.py
+++ ProcessData.py
@@ -20,7 +20,8 @@ class ProcessData:
     remove_n = 50
     n_topics = 10
     n_passes = 20
-
+    matching_files = []
+    
     def __init__(self, languages, years, min_count = 5, remove_n = 50, n_topics = 10, n_passes = 20):
 
         self.languages = languages
@@ -33,7 +34,28 @@ class ProcessData:
 
     def find_matching_files(self):
         '''
-        This function searches for all the filepaths in the provided data path
+        This function creates a dictionary containing the filepaths for all
+        the files in all the desired languages and desired years
         '''
+        filepaths = {}
+        for language in self.languages:
+            filepaths[language] = {}
+        for language in self.languages:
+            for year in self.years:
+                filepaths[language][year] = get_filepaths(
+                                            "UN/{}/{}".format(language, year))
+                filepaths[language][year] = [file[file.index('\\')+1:] 
+                                            for file in filepaths[language][year]]
+        matching_files = {}
+        for year in self.years:
+            set1 = set(filepaths["English"][year])
+            set2 = set(filepaths["Arabic"][year])
+            set3 = set(filepaths["Chinese"][year])
+            set4 = set(filepaths["Russian"][year])
+            set5 = set(filepaths["Spanish"][year])
+            set6 = set(filepaths["French"][year])
+            set1.intersection_update(set2, set3, set4, set5, set6)
+            matching_files[year] = list(set1)
+        return matching_files
 
 
