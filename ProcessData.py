0765bdb25937c8ec651275010dea75d730c95f11
Deepak Gupta <dgupta@Deepaks-MacBook-Pro.local>
2019-03-10 14:53:23 +0100

    started creating OOP based project

---
+++ ProcessData.py
@@ -0,0 +1,12 @@
+"""
+Script: This class contains all the attributes and functions required for preprocessing of the data before
+performing linguistic relativity
+Author: Debjani Bhowmick, Tilburg University
+Year: 2019
+"""
+
+
+class ProcessData:
+
+    # Defining the attributes of the class
+    min_count = 5 # Defines the minimum number of documents that a certain word needs to be present to be considered for study
---
+++ test_run1.py
@@ -0,0 +1,13 @@
+'''
+script: Test run 1, to be edited later
+Author: Debjani Bhowmick, Tilburg University
+'''
+
+# importing necessary libraries
+
+
+# Languages and years of data to be used for the study
+languages = ["french", "spanish"]
+years = ["1990", "1991"]
+
+# Parameters to be used in the study
